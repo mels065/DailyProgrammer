bff2324c4dd76e1bc8eea868a3b852127d2677e3
Brandon Mellus <mellusbrandon@gmail.com>
2015-02-04 18:18:47 -0500

    Make word1 and word2 into attributes for Game object; privatize all current methods and attributes

--- wordswithenemies/cannon.rb
+++ wordswithenemies/cannon.rb
@@ -1,26 +1,28 @@
 class Game
+	private
+	
+	@word1 = ""
+	@word2 = ""
+	
 	def battle(word1, word2)
-		word1.downcase!
-		word2.downcase!
+		@word1 = word1.downcase
+		@word2 = word2.downcase
 		
-		word1.each_char do |c|
-			if word2.include?(c)
-				w1_i = word1.index(c)
-				w2_i = word2.index(c)
-				word1 = word1[0...w1_i] + word1[w1_i+1...word1.length]
-				word2 = word2[0...w2_i] + word2[w2_i+1...word2.length]
+		@word1.each_char do |c|
+			if @word2.include?(c)
+				w1_i = @word1.index(c)
+				w2_i = @word2.index(c)
+				@word1 = @word1[0...w1_i] + @word1[w1_i+1...word1.length]
+				@word2 = @word2[0...w2_i] + @word2[w2_i+1...word2.length]
 			end
 		end
-		
-		return [word1, word2]
 	end
 	
-	def result(debris1, debris2)
-		#debris1 and debris2 are strings
-		puts debris1 + debris2
-		if debris1.length > debris2.length
+	def result
+		puts @word1 + @word2
+		if @word1.length > @word2.length
 			puts "Word 1 wins!"
-		elsif debris1.length < debris2.length
+		elsif @word1.length < @word2.length
 			puts "Word 2 wins!"
 		else
 			puts "It's a tie!"
